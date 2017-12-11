ef338f9df7a79858db68623e845fb0338b5b75d1
Oleksandr Solovov <oleksandr.solovov@globallogic.com>
2017-12-11 14:30:31 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ set fish_greeting ""
 
 set -x TERM "xterm-256color"
 
+#locale
 set -x LANG "en_US.UTF-8"
 set -x LC_CTYPE "en_US.UTF-8"
 set -x LC_ALL "en_US.UTF-8"
@@ -13,15 +14,31 @@ set -gx PATH "$HOME/bin" $PATH
 # colored GCC warnings and errors
 set -gx GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
+#XDG
+# System
+set -x XDG_DATA_DIRS /usr/share /usr/local/share
+set -x XDG_CONFIG_DIRS /etc/xdg
+
+# User
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_DESKTOP_DIR $HOME/Desktop
+set -x XDG_DOWNLOAD_DIR $HOME/Downloads
+set -x XDG_DOCUMENTS_DIR $HOME/Documents
+set -x XDG_MUSIC_DIR $HOME/Music
+set -x XDG_PICTURES_DIR $HOME/Pictures
+set -x XDG_VIDEOS_DIR $HOME/Videos
+
 #navigation
 function ... ; cd ../.. ; end
 function .... ; cd ../../.. ; end
 function ..... ; cd ../../../.. ; end
 
-function l ; ls -CF ; end
-function ll ; ls -alF ; end
-function la ; ls -A ; end
-function lc ; colorls ; end
+alias l="ls -CF"
+alias ll="ls -alF"
+alias la="ls -A"
+alias lc="colorls"
 
 function generate_tags --description "Generate ctags and cscope for C"
 	find -name '*.[hc]' -exec ctags '{}' + ; find -name '*.[hc]' -exec cscope -b '{}' +
