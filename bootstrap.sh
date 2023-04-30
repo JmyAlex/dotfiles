ddf49dde8091d2c07f66bc6717aa6bdc8ffa4c6e
Oleksandr Solovov <oleksandr.solovov@celeno.com>
2023-04-30 22:45:43 +0300

    cleanup files

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,25 +6,13 @@ set -e
 declare -a MY_STUFF=(
     wireshark
     vlc
-    cscope
-    exuberant-ctags
     iperf
-    rxvt-unicode-256color
     ranger
-    vim
     tmux
-    silversearcher-ag
     htop
-    minicom
-    isc-dhcp-server
-    atftpd
-    subversion
     git
-    meld
     xsel
     xclip
-    tree
-    wmctrl
     mc
     ssh
     fortune
@@ -35,58 +23,27 @@ declare -a MY_STUFF=(
     fish
     tilix
     copyq
-    lxappearance
-    pavucontrol
-    arc-theme
-    papirus-icon-theme
-    feh
 )
 
 declare -a DEV_PACK=(
-    bison
     build-essential
     gcc
-    fakeroot
-    gettext
-    gperf
-    libncurses5
-    libncurses5-dev
     make
-    texinfo
-    zlib1g-dev
+    meld
+    subversion
+    minicom
+    isc-dhcp-server
+    atftpd
+    cscope
+    exuberant-ctags
     gawk
-    flex
-    doxygen
-    tcllib
     expect
-    unifdef
-    pkg-config
     vlan
     wget
     git-svn
     git-core
-    diffstat
     unzip
     gcc-multilib
-    chrpath
-    desktop-file-utils
-    groff
-    libxml-parser-perl
-    libsdl1.2-dev
-    xterm
-    libgl1-mesa-dev
-    libglu1-mesa-dev
-    xsltproc
-    docbook-utils
-    fop
-    dblatex
-    xmlto
-    autoconf
-    automake
-    libtool
-    libglib2.0-dev
-    squashfs-tools
-    colordiff
     valgrind
 )
 
@@ -95,6 +52,7 @@ declare -a DEV_PACK=(
 # bat
 # duf
 # fd-find
+# sd
 # git-delta
 # htop
 # iotop
@@ -107,9 +65,6 @@ declare -a DEV_PACK=(
 sudo apt-get update
 sudo apt-get -y upgrade
 
-#intall tex
-#sudo apt-get install --no-install-recommends -y texlive-latex-extra pgf latex-xcolor latex-beamer
-
 echo "Installing packages..."
 sudo apt-get install -y ${MY_STUFF[@]}
 sudo apt-get install -y ${DEV_PACK[@]}
@@ -120,7 +75,6 @@ echo "Configuration..."
 
 mkdir -p $HOME/bin
 mkdir -p $HOME/Github
-mkdir -p $HOME/.config/fish/functions
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/tilix/schemes
 
@@ -130,11 +84,6 @@ git clone https://github.com/JmyAlex/dotfiles.git $HOME/Github/dotfiles
 
 # tmux plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# vim plugins
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #FIXME install font https://github.com/belluzj/fantasque-sans
 
@@ -145,79 +94,35 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 mv ~/.bashrc ~/.bashrc_default_backup
 mv ~/.profile ~/.profile_default_backup
 
-#FIXME add to fstab
-#/dev/sdb1 /mnt/sdb1              ext4    relatime,barrier=0 0       2
-
-#FIXME copy compilers to /opt
-
 echo "Symlinking dot files..."
 
-ln -s ~/Github/dotfiles/.agignore ~/.agignore
+ln -s ~/Github/dotfiles/.agignore ~/.rgignore
+ln -s ~/Github/dotfiles/.gitconfig ~/.gitconfig
+
 ln -s ~/Github/dotfiles/.bashrc ~/.bashrc
 ln -s ~/Github/dotfiles/.profile ~/.profile
 ln -s ~/Github/dotfiles/.inputrc ~/.inputrc
-ln -s ~/Github/dotfiles/.Xresources ~/.Xresources
-
-ln -s ~/Github/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Github/dotfiles/.shell_aliases ~/.shell_aliases
 ln -s ~/Github/dotfiles/.shell_functions ~/.shell_functions
 ln -s ~/Github/dotfiles/.shell_promt ~/.shell_promt
+
 ln -s ~/Github/dotfiles/tmux_airline ~/.tmux_airline
 ln -s ~/Github/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/Github/dotfiles/.vimrc ~/.vimrc
 ln -s ~/Github/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/Github/dotfiles/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 ln -s ~/Github/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 cp -r ~/Github/dotfiles/.config/tilix/schemes/* ~/.config/tilix/schemes
 
-#bspwm
-mkdir -p $HOME/.config/bspwm
-mkdir -p $HOME/.config/sxhkd
-mkdir -p $HOME/.config/polybar
-mkdir -p $HOME/.config/dunst
-mkdir -p $HOME/.config/rofi
-ln -s ~/Github/dotfiles/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-ln -s ~/Github/dotfiles/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-ln -s ~/Github/dotfiles/.config/polybar/config ~/.config/polybar/config
-ln -s ~/Github/dotfiles/.config/dunst/dunstrc ~/.config/dunst/dunstrc
-ln -s ~/Github/dotfiles/.compton.conf ~/.compton.conf
-ln -s ~/Github/dotfiles/.config/rofi/config.rasi ~/.config/rofi/config.rasi
-ln -s ~/Github/dotfiles/.config/rofi/onedark.rasi ~/.config/rofi/onedark.rasi
-
-cp -r ~/Github/dotfiles/.urxvt ~/
 mkdir -p $HOME/.local/share/mc/skins/
 ln -s ~/Github/dotfiles/dracula.ini ~/.local/share/mc/skins/dracula.ini
-#cp -r ~/Github/dotfiles/.config/ranger ~/.config/
-
-vim +PlugInstall +PlugUpdate +qall
-
-mkdir -p ~/.vim/tmp/{undo,backup,swap}
-
-#cp ~/Github/dotfiles/tomorrow.vim ~/.vim/bundle/vim-airline/autoload/airline/themes
 
 #wireshark without sudo
 sudo dpkg-reconfigure wireshark-common
 sudo usermod -a -G wireshark `whoami`
 sudo usermod -a -G dialout `whoami`
 
-sudo dpkg-reconfigure dash
-
 cat <<-EOF
 TODO ...
     Install font
-    Add hdd to fstab
-    Copy compilers
-    dash -> bash
-    configure dhcp server
-    sudo update-alternatives --install /usr/bin/make make /usr/bin/make3.8 10
-    sudo update-alternatives --install /usr/bin/make make /usr/bin/make4.1 20
-    sudo update-alternatives --config make
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 10
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
-    sudo update-alternatives --config gcc
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-3.4 40 --slave /usr/bin/g++ g++ /usr/bin/g++-3.4
-    sudo update-alternatives --config gcc
 EOF
 
 #atftpd config
